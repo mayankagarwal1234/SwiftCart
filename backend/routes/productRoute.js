b56d2b80338ce0866919a9763b94ba86ac8ade93
Mayank Agarwal <142929113+mayankagarwal1234@users.noreply.github.com>
2025-05-17 15:10:37 +0530

    Update productRoute.js

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,13 +1,35 @@
-import express from "express"
-import {listProduct,removeProduct,singleProduct,addProduct } from "../controllers/productController.js"
-import upload from "../middleware/multer.js"
-import adminAuth from "../middleware/adminauth.js"
+import express from "express";
+import {
+  listProducts,
+  removeProduct,
+  getSingleProduct,
+  addProduct,
+} from "../controllers/productController.js";
+import upload from "../middleware/multer.js";
+import adminAuth from "../middleware/adminauth.js";
 
-const productRouter =express.Router()
-productRouter.post('/add',adminAuth,upload.fields([{name:'image1',maxCount:1},{name:'image2',maxCount:1},{name:'image3',maxCount:1},{name:'image4',maxCount:1}]),addProduct)
-productRouter.post('/remove',adminAuth,removeProduct)
-productRouter.post('/single',singleProduct)
-productRouter.get('/list',listProduct)
+const productRouter = express.Router();
 
+// Add product with image uploads (admin only)
+productRouter.post(
+  "/add",
+  adminAuth,
+  upload.fields([
+    { name: "image1", maxCount: 1 },
+    { name: "image2", maxCount: 1 },
+    { name: "image3", maxCount: 1 },
+    { name: "image4", maxCount: 1 },
+  ]),
+  addProduct
+);
+
+// Remove product (admin only)
+productRouter.post("/remove", adminAuth, removeProduct);
+
+// Get single product info
+productRouter.post("/single", getSingleProduct);
+
+// List all products
+productRouter.get("/list", listProducts);
 
 export default productRouter;
