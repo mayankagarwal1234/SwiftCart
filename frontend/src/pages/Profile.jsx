2ff64cb888b13caa53cd08b59dcb4ee2ce6958a7
Mayank Agarwal <142929113+mayankagarwal1234@users.noreply.github.com>
2025-05-31 06:49:12 +0530

    Update Profile.jsx

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -19,13 +19,13 @@ const Profile = () => {
 
   return (
     <div className="max-w-3xl mx-auto p-8">
-      <h1 className="text-4xl sm:text-5xl font-bold text-center text-black mb-8">
+      <h1 className="text-4xl sm:text-5xl font-bold text-center text-black mb-1">
         Welcome to your Profile
       </h1>
 
-      <div className="bg-white  rounded-2xl p-8 space-y-6 text-center">
+      <div className="bg-white  rounded-2xl p-2 space-y-2 text-center">
         <div>
-          <h2 className="text-2xl font-semibold text-gray-900 mb-2">
+          <h2 className="text-4xl font-semibold text-gray-900 mb-2">
             {user.name}
           </h2>
           <p className="text-lg text-gray-600">{user.email}</p>
