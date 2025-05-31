9f942d1853ce2291aa18c179897e51a56db190c0
Mayank Agarwal <142929113+mayankagarwal1234@users.noreply.github.com>
2025-05-31 06:15:24 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,19 +7,20 @@ import Contact from './pages/Contact'
 import Product from './pages/Product'
 import Cart from './pages/Cart'
 import Orders from './pages/Orders'
+import Profile from './pages/Profile'
 import Verify from './pages/Verify'
 import Placeorder from './pages/Placeorder'
 import Login from './pages/Login'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Searchbar from './components/Searchbar'
-import { ToastContainer, toast } from 'react-toastify'
+import { Toaster } from 'react-hot-toast';
 import 'react-toastify/dist/ReactToastify.css'
 
 const App = () => {
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
-      <ToastContainer/>
+      <Toaster/>
       <Navbar/>
       <Searchbar/>
       <Routes>
@@ -32,6 +33,7 @@ const App = () => {
         <Route path='/login'element={<Login/>}/>
         <Route path='/place-order'element={<Placeorder/>}/>
         <Route path='/orders'element={<Orders/>}/>
+        <Route path='/profile'element={<Profile/>}/>
         <Route path='/verify'element={<Verify/>}/>
       </Routes>
       <Footer/>
