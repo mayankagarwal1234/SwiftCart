ad1a1cbd13fb161e955fd0242409e05f4b7cbef3
Mayank Agarwal <142929113+mayankagarwal1234@users.noreply.github.com>
2025-05-31 06:53:44 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useContext, useState } from "react";
 import { assets } from "../assets/assets";
 import { Link, NavLink } from "react-router-dom";
 import { ShopContext } from "../context/ShopContext";
-import { toast } from "react-toastify";
+import toast from "react-hot-toast";
 
 const Navbar = () => {
   const [visible, setVisible] = useState(false);
