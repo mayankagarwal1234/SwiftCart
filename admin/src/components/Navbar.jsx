92e07d782fdab5890e0ad91b679a9194f723ae69
Mayank Agarwal <142929113+mayankagarwal1234@users.noreply.github.com>
2025-05-31 07:30:35 +0530

    Update Navbar.jsx

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
-import {assets} from '../assets/assets'
+import React from 'react';
+import { assets } from '../assets/assets';
+import toast from 'react-hot-toast';
 
-const Navbar = ({settoken}) => {
+const Navbar = ({ settoken }) => {
   return (
     <div className='flex items-center py-2 px-[4%] justify-between'>
-        <img className="w-[max(10%,80px)]" src={assets.logo} alt="" />
-        <button onClick={()=>settoken('')} className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm'>LogOut</button>
+      <img className="w-[max(10%,80px)]" src={assets.logo} alt="Logo" />
+      <button
+        onClick={() => {
+          settoken('');
+          toast.success("Logout successful!");
+        }}
+        className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm'
+      >
+        LogOut
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
