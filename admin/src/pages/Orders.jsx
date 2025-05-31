0113533594f4efabe043ae4fd60955a311494a47
Mayank Agarwal <142929113+mayankagarwal1234@users.noreply.github.com>
2025-05-31 07:31:46 +0530

    Update Orders.jsx

--- admin/src/pages/Orders.jsx
+++ admin/src/pages/Orders.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { toast } from "react-toastify";
+import toast from "react-hot-toast";
 import { backendUrl, currency } from "../App";
 import { assets } from "../assets/assets";
 
